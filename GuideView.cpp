f7d0bac584bcc0f9482ef43cfe95dee7426345d4
silly <zorin2604@gmail.com>
2025-06-16 14:46:11 +0300

    Добавлено немного стиля в webView для субокна GuideView

--- GuideView.cpp
+++ GuideView.cpp
@@ -19,6 +19,7 @@ void GuideView::setMarkdown(const QString& markdown)
 {
     QByteArray utf8 = markdown.toUtf8();
     char *html = cmark_markdown_to_html(utf8.constData(), utf8.size(), CMARK_OPT_DEFAULT);
-    webView->setHtml(QString::fromUtf8(html));
+    webView->setHtml(QString("<style>* {color: rgb(174, 149, 229);} body {background-color: rgb(59, 48, 84);} code {background-color: rgb(87, 70, 123); color: white; padding: 10px; border-radius: 5px; border: 1px solid rgb(174, 149, 229);}</style>") + QString::fromUtf8(html));
+    qDebug() << html;
     free(html);
 }
